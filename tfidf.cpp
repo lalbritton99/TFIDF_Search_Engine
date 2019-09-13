03da713ed80b71f66d58cdb78c6c1e1e9cb09a81
awr0047 <austinreeves@my.unt.edu>
2019-09-12 22:41:28 -0500

    Added two files for the TF-IDF work. Just made the class for this portion.

---
+++ tfidf.cpp
@@ -0,0 +1,30 @@
+#include "tfidf.h"
+
+tf_idf::tf_idf(){										// constructor
+		
+}
+
+void tf_idf::SetID(int idInput){						// mutator for id
+}
+int tf_idf::GetID(){										// accessor for id
+}		
+
+void tf_idf::SetName(string nameInput){					// mutator for name
+}
+string tf_idf::GetName(){								// accessor for name
+}
+	
+void tf_idf::SetTF(int tfInput){						// mutator for tf
+}
+int tf_idf::GetTF(){									// accessor for tf
+}		
+
+void tf_idf::SetIDF(double idfInput){					// mutator for idf
+}
+double tf_idf::GetIDF(){								// accessor for idf
+}
+		
+void tf_idf::SetTFIDF(double tfidfInput){				// mutator for tfidf
+}
+double tf_idf::GetTFIDF(){								// accessor for tfidf
+}
---
+++ tfidf.h
@@ -0,0 +1,31 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+class tf_idf {
+	private:
+		int         id;									// unique id number for every word
+		string      name;								// the exact name of the unique word
+		int         tf;									// the term frequency for a given unique word (a running count)
+		double      idf;								// the inverse document frequency for a given unique word
+		double      tfidf;								// the term freq. multiplied by the inverse doc. freq.
+		vector<int> docsAppearedIn;						// dynamic list of all the documents a given word appears in
+		
+	public:
+		tf_idf();										// constructor
+		
+		void SetID(int idInput);						// mutator for id
+		int GetID();									// accessor for id
+		
+		void SetName(string nameInput);					// mutator for name
+		string GetName();								// accessor for name
+		
+		void SetTF(int tfInput);						// mutator for tf
+		int GetTF();									// accessor for tf
+		
+		void SetIDF(double idfInput);					// mutator for idf
+		double GetIDF();								// accessor for idf
+		
+		void SetTFIDF(double tfidfInput);				// mutator for tfidf
+		double GetTFIDF();								// accessor for tfidf
+};
