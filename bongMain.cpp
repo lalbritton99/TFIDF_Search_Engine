403d41169dbf1a3f71f42c90b6469bec38f38515
Marcus Summers <mds0390@unt.edu>
2019-09-25 14:03:35 -0500

    Added file name reading to main loop

--- bongMain.cpp
+++ bongMain.cpp
@@ -1,14 +1,67 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <fstream>
 using namespace std;
 
-#include "tfidf.h"
+//#include "tfidf.h"
+//#include "document.h"
+//#include "documentCol.h"
 
-int main() {
-	
-	
-	
-	
+int main()
+{
+	string documentsFile = "";
+	string stopwordsFile = "";
+	ifstream documents;
+	ifstream stopwords;
+
+	// gets documents file from user
+	while(true)
+	{
+		cout << endl;
+		cout << "Documents file name: ";
+		cin >> documentsFile;
+
+		// fixme
+		cout << "c: " << documentsFile << endl;
+
+		documents.open(documentsFile);
+
+		if (!documents.is_open())
+		{
+			cout << endl;
+			cout << "--- Documents file " << documentsFile << " does not exist. Try Again ---" << endl;
+		}
+		else
+		{
+			break;
+		}
+	}
+
+	// gets stopwords file from user
+	while(true)
+	{
+		cout << endl;
+		cout << "Stopwords file name: ";
+		cin >> stopwordsFile;
+
+		// fixme
+		cout << "s: " << stopwordsFile << endl;
+
+		stopwords.open(stopwordsFile);
+
+		if (!stopwords.is_open())
+		{
+			cout << endl;
+			cout << "--- Stopwords file " << stopwordsFile << " does not exist. Try Again ---" << endl;
+		}
+		else
+		{
+			break;
+		}
+	}
+
+	//
+
 	return 0;
 }
