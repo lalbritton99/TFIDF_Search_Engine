df1d5e41e14b7a2ca21dcf28c5ad817a5e4a38dc
Logan Albritton <loganalbritton@my.unt.edu>
2019-10-01 16:44:47 -0500

    Added cosine similarity function declaration

--- tfidfCol.h
+++ tfidfCol.h
@@ -15,11 +15,12 @@ class tf_idfCol {
 		void FindTFIDF();							// function to find the tf-idf for every word
 		void Print();								// prints out the tf, idf, tf-idf, and the word for every unique word in the first document 
 		
-		double dotProduct(tf_idfCol secondSet);					// returns the Dot product between the TFIDF values of this set and another passed in. numerator of cosine similarity equation
+		double dotProduct(tf_idfCol secondSet);					// Returns the Dot product between the TFIDF values of this set and another passed in. numerator of cosine similarity equation
 		double squareProduct(tf_idfCol secondSet);				// Returns the product of the square roots of the sums of the squares of the TFIDF values of this set and another passed in. denomonator of cosine similarity equation
+		double cosineSimilarity(tf_idfCol secondSet);				// Returns the cosine similarity between the TFIDF collection obj and another passed in
 
 		int getTFIDFVecSize();							// returns size of TFIDF vector
-		tf_idf getTFIDFObj(int index);						// returns TFIDF at given index in TFIDF vector
+		tf_idf getTFIDFObj(int index);						// returns TFIDF at given index in TFIDF vector. Accessor function
 	
 };
 
