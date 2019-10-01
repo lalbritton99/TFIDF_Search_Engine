6ce4fb134e6d60b251aaa69a341bfd27ca7283d9
Logan Albritton <loganalbritton@my.unt.edu>
2019-10-01 10:49:45 -0500

    Added acessors functions for TFIDF collection vector

--- tfidfCol.h
+++ tfidfCol.h
@@ -2,13 +2,13 @@
 
 class tf_idfCol {
 	private:
-		vector<tf_idf>tfidfVec;						// collection of tf_idf objects
+		vector<tf_idf>tfidfVec;							// collection of tf_idf objects
 		int firstDocID;								// the id number of the first document that gets read in // maybe dont need???
 	
 	public:
 		tf_idfCol();								// constructor
-		void SetFirstDocID(int firstDocIdInput);	// mutator for firstDocId
-		int GetFirstDocID();						// accessor for firstDocID
+		void SetFirstDocID(int firstDocIdInput);				// mutator for firstDocId
+		int GetFirstDocID();							// accessor for firstDocID
 	
 		void FindTF();								// function to find the tf for every word
 		void FindIDF();								// function to find the idf for every word
@@ -18,6 +18,8 @@ class tf_idfCol {
 		double dotProduct(tf_idfCol secondSet);					// returns the Dot product between the TFIDF values of this set and another passed in. numerator of cosine similarity equation
 		double squareProduct(tf_idfCol secondSet);				// Returns the product of the square roots of the sums of the squares of the TFIDF values of this set and another passed in. denomonator of cosine similarity equation
 
-
+		int getTFIDFVecSize();							// returns size of TFIDF vector
+		tf_idf getTFIDF(int index);						// returns TFIDF at given index in TFIDF vector
+	
 };
 
