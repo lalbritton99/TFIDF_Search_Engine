cf52bc7d9ed6516c6932a8df53e58ff86d4fcd59
Logan Albritton <loganalbritton@my.unt.edu>
2019-10-03 13:59:13 -0500

    Part 6 finished until testing possible

--- bongMain.cpp
+++ bongMain.cpp
@@ -96,9 +96,8 @@ int main() {
 
 	for(int x=0; x<tfidfColVec.size(); x++){				// loop through TFIDF Collection Vector
 		double tempCosSim = 0;
-		//tempCosSim = [QUERY_TFIDFCOL_OBJECT[x?]].cosineSimilarity(tfidfColVec[x]);	//FIXME
-		//                    		   ???^^???  ?obj or vec?
-		tfidfColVec[x].SetCosineSimilarity(tempCosSim);
+		//tempCosSim = tfidfQueryVec[x].cosineSimilarity(tfidfColVec[x]);	//FIXME  calculates cosine similarity between TFIDF of doc and TFIDF of correlated query TFIDF
+		tfidfColVec[x].SetCosineSimilarity(tempCosSim); 			// set cosine similariry of doc to query in TFIDF obj for doc
 	}
 
 //--------------------------------------------- TFIDF Collection Vector Sort -----------------------------------------------------
