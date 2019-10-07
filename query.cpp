ca886bc40b860c481ce936269b5eb5fc87599bb9
jek0138 <jek0138@unt.edu>
2019-10-07 16:31:56 -0500

    Commented query code

--- query.cpp
+++ query.cpp
@@ -4,22 +4,22 @@ vector<string> query(vector<string> stops){
         string query, tempQuery;
 	int temp = 0;
 	bool stopped = true;
-	vector<string> queryVec;
-	vector<string> stoppedQuery;
-        cout << "Enter your query: ";
+	vector<string> queryVec; //Vector of strings for query as input
+	vector<string> stoppedQuery; //Vector to store query after the stopwords are removed
+        cout << "Enter your query: "; //Prompt for the query
 	cin.ignore();
-        getline(cin, query);
+        getline(cin, query); //Read in the query
 	
-	istringstream querie(query);
+	istringstream queryStream(query); //String stream to iterate through the string
 
-	while(querie >> tempQuery) {
+	while(queryStream >> tempQuery) { //While loop to populate queryVec
 		queryVec.push_back(tempQuery);
 	}
 
 	vector<string>::iterator itr;
-	for(itr = queryVec.begin(); itr < queryVec.end(); itr++){
-		for(int j = 0; j < stops.size(); j++){
-			if(*itr == stops[j]){
+	for(itr = queryVec.begin(); itr < queryVec.end(); itr++){ //Loops through queryVec
+		for(int j = 0; j < stops.size(); j++){ //Loops through stop word vector
+			if(*itr == stops[j]){ //If the element is found in the stop word query, set stopped to true
 				stopped = true;
 				break;
 			}
@@ -27,17 +27,17 @@ vector<string> query(vector<string> stops){
 				stopped = false;
 			}
 		}
-		if (!stopped){
+		if (!stopped){ //If stopped is false, add the element to the stopped query
 			stoppedQuery.push_back(*itr);
 		}
 	}
-	for(int i = 0; i < stoppedQuery.size(); i++){
-		for(int j = 0; j < stoppedQuery[i].size(); j++){
-			if((ispunct(stoppedQuery[i].at(j))) && (stoppedQuery[i].at(j) != '-')){
+	for(int i = 0; i < stoppedQuery.size(); i++){ //Loop through stopped query
+		for(int j = 0; j < stoppedQuery[i].size(); j++){ //Loop through each individual words
+			if((ispunct(stoppedQuery[i].at(j))) && (stoppedQuery[i].at(j) != '-')){ //If punctuation is found, remove it
 				stoppedQuery[i].replace(stoppedQuery[i].size()-1, 1, "");
 			}
 		}
 	}
-	return stoppedQuery;
+	return stoppedQuery; //Return a copy of the query without stop words
 }
 
